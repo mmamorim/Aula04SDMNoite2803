289b5b7ece596b2a95b0066e8adc2f071ed47f67
mmamorim <marc.m.amorim@gmail.com>
2022-03-28 20:19:44 -0300

    express adicionado

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 
 let util = require("./uteis");
 
-console.log("Oi gente");
+console.log("🚀🚀🚀 Iniciando meu servidor 🚀🚀🚀");
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/teste', (req, res) => {
+  res.send('Você acessou a rota /teste')
+})
+
+app.listen(port, () => {
+  console.log(`😀😀 Servidor iniciado...escutando na porta ${port}`)
+})
--- uteis.js
+++ uteis.js
@@ -1,13 +1,20 @@
 
 module.exports = {
 
-    ehPrimo: function(valor) {
-        for(let i=2; i<valor/2; i++) {
-            if(valor%i == 0) {
+    ehPrimo: function (valor) {
+        for (let i = 2; i < valor / 2; i++) {
+            if (valor % i == 0) {
                 return false;
             }
         }
         return true;
+    },
+
+    umTesteString: () => {
+        let nome = "Marcelo"
+        let idade = 15
+        let texto = `dwdwd ${idade} do ${nome} dfdf`
+        return texto;
     }
 
 } 
